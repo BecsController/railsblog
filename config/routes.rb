463bbca21bf7ca1298f278a64e9057f740addc47
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-10-07 16:10:20 +1300

    routes for sessions and controller actions for create and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
 
   resources :users
 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   root 'welcome#index'
 end
