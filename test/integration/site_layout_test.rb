a2bb621b455f40c12d04fd6a655b1bcd8a5291bd
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-10-09 16:40:11 +1300

    controller testing, link testing, added footer

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+    get root_path
+    assert_template 'welcome/index'
+    assert_select "a[href=?]", root_path, count: 1
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
+  end
+end
